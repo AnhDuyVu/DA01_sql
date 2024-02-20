7db2ed0c87aaa1a90a81b3b13bbd965fe0d6250e
Vu Anh Duy <119872105+AnhDuyVu@users.noreply.github.com>
2024-02-20 23:01:44 +0700

    Update Practice_01.sql

--- Practice_01.sql
+++ Practice_01.sql
@@ -1,5 +1,23 @@
----EX01
+---EX1
 Select name from city
 where countrycode = 'USA' and population > 120000;
----EX02
+
+---EX2
+Select * from city
+where countrycode = 'JPN';
+
+---EX3
+Select city, state from station;
+
+---EX4
+Select distinct city from station
+where city like 'a%' or city like 'e%' or city like 'i%' or city like 'o%' or city like 'u%';
+
+---EX5
+Select distinct city from station
+where city like '%a' or city like '%e' or city like '%i' or city like '%o' or city like '%u';
+
+---EX6
+Select distinct city from station
+where city not like 'a%' and city not like 'e%' and city not like 'i%' and city not like 'o%' and city not like 'u%'; 
 
