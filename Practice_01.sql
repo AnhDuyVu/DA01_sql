92c821fa26e4c46f43b4eebd85e786e6511cee8a
Vu Anh Duy <119872105+AnhDuyVu@users.noreply.github.com>
2024-02-20 23:21:04 +0700

    Update Practice_01.sql

--- Practice_01.sql
+++ Practice_01.sql
@@ -21,3 +21,15 @@ where city like '%a' or city like '%e' or city like '%i' or city like '%o' or ci
 Select distinct city from station
 where city not like 'a%' and city not like 'e%' and city not like 'i%' and city not like 'o%' and city not like 'u%'; 
 
+---EX7
+Select name from employee
+order by name;
+
+---EX8
+Select name from employee
+where salary > 2000 and months <10
+order by employee_id asc;
+
+---EX9
+
+
