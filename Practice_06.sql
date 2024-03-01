cf6b99f50d4c93f97f4388151c52e46b2a922786
Vu Anh Duy <119872105+AnhDuyVu@users.noreply.github.com>
2024-03-01 20:51:43 +0700

    Update Practice_06.sql

--- Practice_06.sql
+++ Practice_06.sql
@@ -62,6 +62,74 @@ GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
 
 --ex06
 
+Select month, country, 
+count(trans_date) as trans_count,
+sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from (Select DATE_FORMAT(trans_date, '%Y-%m') as month,
+country,
+state,trans_date, amount
+from Transactions) as state_table
+group by month, country;
+
+--ex07
+
+Select product_id,
+year as first_year,
+quantity, price
+from Sales
+where (product_id,year) in (Select product_id, min(year) as first_year from sales group by product_id);
+
+--ex08
+
+select customer_id
+from Customer
+group by customer_id
+having count(distinct product_key) = 
+    (select count(distinct product_key) from Product);
+
+--ex09
+
+Select employee_id
+from Employees
+where salary < 30000 and manager_id not in (select employee_id from Employees)
+order by employee_id asc;
+
+--ex11
+
+(Select name as results
+from (Select u.name,
+count(rating) as count_rating
+from MovieRating as m
+inner join Users as u
+on m.user_id = u.user_id
+group by u.name
+order by count_rating desc, u.name asc) as username_highest_number_of_rating
+limit 1)
+union all
+(Select title as results
+from (Select mo.title,
+avg(rating) as avg_rating
+from MovieRating as m
+inner join Movies as mo
+on m.movie_id = mo.movie_id
+where DATE_FORMAT(created_at, '%Y-%m') = '2020-02'
+group by mo.title
+order by avg(rating) desc, title asc) as movie_name_highest_avg_rating
+limit 1);
+
+--ex12
+select id, count(*) as num  
+from (select requester_id as id from RequestAccepted
+union all
+select accepter_id as id from RequestAccepted) as friend_table 
+group by id 
+order by count(*) desc 
+limit 1;
+
+
+
 
 
 
