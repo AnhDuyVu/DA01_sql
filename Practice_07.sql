6a75ec8f4325db10995cbfaeb15fc8e051722e94
Vu Anh Duy <119872105+AnhDuyVu@users.noreply.github.com>
2024-03-02 23:25:45 +0700

    Update Practice_07.sql

--- Practice_07.sql
+++ Practice_07.sql
@@ -17,3 +17,28 @@ from (Select card_name,
        first_value(issued_amount) over(PARTITION BY card_name order by issue_year asc, issue_month asc) as first_issued_amount
 from monthly_cards_issued) as a
 order by first_issued_amount desc;
+
+--ex03
+Select user_id,
+       spend,
+       transaction_date
+from (SELECT user_id,
+spend,
+transaction_date,
+dense_rank() over(PARTITION BY user_id order by user_id,transaction_date) as rank_transaction
+from transactions) as rank_transaction_table
+where rank_transaction =3;
+
+--ex04
+Select transaction_date,
+       user_id,
+       count(product_id) as purchase_count
+from (SELECT *,
+       rank() over(PARTITION BY user_id order by transaction_date desc) as rank_transaction_date
+from user_transactions) as rank_transaction_date_table
+where rank_transaction_date = 1
+group by transaction_date, user_id
+order by transaction_date asc;
+
+--ex05
+
